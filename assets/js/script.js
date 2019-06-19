1dd225ce32146a02d94b59aacd061786cfc6bf23
Mason Short <ironicallyshort737@hotmail.com>
2019-06-19 02:27:41 -0600

    api key and topics added

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let btnArray = ['Zelda', 'Mario', 'Pokemon', 'Banjo Kazooie', 'Donkey Kong', 'Pac-Man']
+
+const apiKey = '&api_key=vpDgs21nCK0VKHhMScTQrZxtF5DMTXpi';
+
+const queryUrl = 'http://api.giphy.com/v1/gifs/search?q=';
